a1a3d2827dc55b1769c8c97ea3d5fa34b3c8265f
AlSavva <70343642+AlSavva@users.noreply.github.com>
2021-03-28 18:17:20 +0300

    Update leroyru.py

--- Lesson7_HomeWork/leroyparser/spiders/leroyru.py
+++ Lesson7_HomeWork/leroyparser/spiders/leroyru.py
@@ -11,7 +11,7 @@ class LeroyruSpider(scrapy.Spider):
     def __init__(self, query):
         super(LeroyruSpider, self).__init__()
 
-        self.start_urls = []
+        self.start_urls = [] # список стартовых ссылок.
         self.query = query
         for good in query:
             self.start_urls.append(f'https://leroymerlin.ru/search/?q={good}')
@@ -22,18 +22,17 @@ class LeroyruSpider(scrapy.Spider):
             yield response.follow(link, callback=self.parse_item)
         next_page = response.xpath(
             '//div[@class="service-duplicate-panel-wrapper"]//div[@class="next-paginator-button-wrapper"]/a/@href').extract_first()  # т.к. результат response - SelectorList, используем extract_first()
-        print()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
     def parse_item(self, response: HtmlResponse):
         loader = ItemLoader(item=LeroyparserItem(), response=response)
-        loader.add_value('main', response.request.headers['Referer'])
+        loader.add_value('main', response.request.headers['Referer']) # данные для формирования дирректорий файлов
         loader.add_value('general', response.xpath(
-            '//div[@data-rel="js-detail-product-page"]').attrib)  # .attrib
+            '//div[@data-rel="js-detail-product-page"]').attrib)  # словарь-информация о товаре(все находится в атрибутах)
         loader.add_xpath('characteristic',
-                         '//div[@class="def-list__group"]//*/text()')  # .extract()
+                         '//div[@class="def-list__group"]//*/text()')  # характеристики товара
         loader.add_xpath('photos',
-                         '//uc-pdp-media-carousel//source[contains(@media, "(min-width: 1024px)")]/@data-origin')
-        loader.add_value('url', response.url)  # .extract()
+                         '//uc-pdp-media-carousel//source[contains(@media, "(min-width: 1024px)")]/@data-origin') # full-size фотографии товара
+        loader.add_value('url', response.url)
         yield loader.load_item()
